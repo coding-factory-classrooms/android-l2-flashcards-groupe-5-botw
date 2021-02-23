5d88b287d10226e48db5ac034e8c0516a8417fe3
DogSobre <juanfernandoherrada13@gmail.com>
2021-02-23 17:48:20 +0100

    design of our main layout and question page

---
+++ app/src/main/java/com/aph/flashcard_botw/FlashCardActivity.java
@@ -0,0 +1,14 @@
+package com.aph.flashcard_botw;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class FlashCardActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_flash_card);
+    }
+}
