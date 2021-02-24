924fc63c3e1ee8d8f410dd011527e764c358e867
Marion <meurantmarion@gmail.com>
2021-02-24 21:35:27 +0100

    start list question page. Layout finish

---
+++ app/src/main/java/com/aph/flashcard_botw/QuestionsListActivity.java
@@ -0,0 +1,14 @@
+package com.aph.flashcard_botw;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class QuestionsListActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_questions_list);
+    }
+}
