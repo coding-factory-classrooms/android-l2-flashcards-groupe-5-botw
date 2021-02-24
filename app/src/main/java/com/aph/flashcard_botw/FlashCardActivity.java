ffb89605730ab0eb10272b2776091fb032c2a1fe
Marion <meurantmarion@gmail.com>
2021-02-24 16:14:55 +0100

    Retrieve selectedButton text and add message

--- app/src/main/java/com/aph/flashcard_botw/FlashCardActivity.java
+++ app/src/main/java/com/aph/flashcard_botw/FlashCardActivity.java
@@ -3,10 +3,13 @@ package com.aph.flashcard_botw;
 import android.content.Context;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.View;
+import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.RadioButton;
 import android.widget.RadioGroup;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -14,6 +17,7 @@ import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
 import org.json.simple.parser.JSONParser;
+import org.w3c.dom.Text;
 
 import java.io.IOException;
 import java.io.InputStream;
@@ -21,6 +25,10 @@ import java.util.ArrayList;
 import java.util.Collections;
 
 public class FlashCardActivity extends AppCompatActivity {
+
+    private RadioGroup radioGroup;
+    private RadioButton answerButton;
+    private Button nextQuestionButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -73,8 +81,42 @@ public class FlashCardActivity extends AppCompatActivity {
             int id = context.getResources().getIdentifier(image, "drawable", context.getPackageName());
             questionImage.setImageResource(id);
 
+            addListenerOnButton(goodAnswer);
+
         } catch (JSONException e) {
             e.printStackTrace();
         }
+    }
+
+    public void addListenerOnButton(String goodAnswer) {
+        radioGroup = (RadioGroup) findViewById(R.id.answerRadioGroup);
+        nextQuestionButton = (Button) findViewById(R.id.nextQuestionButton);
+
+        nextQuestionButton.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View v) {
+
+                int radioButtonID = radioGroup.getCheckedRadioButtonId();
+
+                Log.i("radioButton", radioButtonID + "");
+                if (radioButtonID == -1) {
+                    Toast.makeText(FlashCardActivity.this, "Veuillez choisir une réponse", Toast.LENGTH_SHORT).show();
+                    return;
+                }
+                RadioButton radioButton = radioGroup.findViewById(radioButtonID);
+                String selectedAnswer = (String) radioButton.getText();
+
+                TextView responseTextView = findViewById(R.id.responseTextView);
+
+                if (selectedAnswer == goodAnswer) {
+                    responseTextView.setText("Tintintintin");
+                }
+                else {
+                    responseTextView.setText("T'es pourri mec");
+                }
+
+            }
+        });
     }
 }
