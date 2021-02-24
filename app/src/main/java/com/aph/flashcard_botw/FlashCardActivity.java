78d42504a47c35e757800a57e52503507f11110a
Marion <meurantmarion@gmail.com>
2021-02-24 12:25:09 +0100

    Change json reader

--- app/src/main/java/com/aph/flashcard_botw/FlashCardActivity.java
+++ app/src/main/java/com/aph/flashcard_botw/FlashCardActivity.java
@@ -5,12 +5,15 @@ import android.util.Log;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import org.json.JSONException;
+import org.json.JSONObject;
 import org.json.simple.JSONArray;
 import org.json.simple.parser.JSONParser;
 import org.json.simple.parser.ParseException;
 
 import java.io.BufferedReader;
 import java.io.IOException;
+import java.io.InputStream;
 import java.io.InputStreamReader;
 
 public class FlashCardActivity extends AppCompatActivity {
@@ -22,7 +25,26 @@ public class FlashCardActivity extends AppCompatActivity {
 
         JSONParser jsonP = new JSONParser();
 
+        String string = "";
         try {
+            InputStream inputStream = getAssets().open("questions.json");
+            int size = inputStream.available();
+            byte[] buffer = new byte[size];
+            inputStream.read(buffer);
+            string = new String(buffer);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        try {
+            JSONObject jsonObject = new JSONObject(string);
+            JSONObject bokoblin = jsonObject.getJSONObject("Bokoblin");
+            Log.i("jsonobject", bokoblin + "");
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
+        Log.i("string", string);
+
+       /* try {
             InputStreamReader jsonFile = new InputStreamReader(getAssets().open("questions.json"));
             BufferedReader bReader = new BufferedReader(jsonFile);
             String line = bReader.readLine();
@@ -38,6 +60,6 @@ public class FlashCardActivity extends AppCompatActivity {
 
         } catch (IOException | ParseException e) {
             Log.e("ReadJsonFailed", String.valueOf(e));
-        }
+        }*/
     }
 }
